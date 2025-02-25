4f3f9558fe8069f7963cc29036814d9d5f282fca
DESKTOP-75OBE8B\Ahmet <burak.agca@evatro.com>
2025-02-25 18:27:57 +0300

    End of 13th Phase

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -8,6 +8,7 @@ public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -18,5 +19,9 @@ public class DataContext(DbContextOptions options) : DbContext(options)
         builder.Entity<UserLike>().HasOne(s => s.SourceUser).WithMany(l => l.LikedUsers).HasForeignKey(s => s.SourceUserId).OnDelete(DeleteBehavior.Cascade);
 
         builder.Entity<UserLike>().HasOne(s => s.TargetUser).WithMany(l => l.LikedByUsers).HasForeignKey(s => s.TargetUserId).OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>().HasOne(x => x.Recipient).WithMany(x => x.MessagesReceived).OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Message>().HasOne(x => x.Sender).WithMany(x => x.MessagesSent).OnDelete(DeleteBehavior.Restrict);
     }
 }
